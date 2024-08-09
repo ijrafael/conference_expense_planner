1d0b6c818d4cd4c7448645de55d825f069a05fec
ijrafael <irvinjrafael@gmail.com>
2024-08-09 17:04:23 -0400

    changing repo location

--- learning_react/vite.config.js
+++ learning_react/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/conference_express_planner",
+    base: "/learning_react",
   plugins: [react()],
 })
